050f64e4724b8902d3ce8254253ea7ad15af7557
Soumyabrata Nandi <Soumyabrata Nandi@192.168.137.189>
2024-11-06 12:47:01 +0530

    pushing

--- ConcastCopyFramework/src/test/java/trialpackage/ProductPage.java
+++ ConcastCopyFramework/src/test/java/trialpackage/ProductPage.java
@@ -5,7 +5,7 @@ public class ProductPage {
 	public static void main(String[] args) {
 		System.out.println("i have added new product page");
 
- System.out.println("Added by sayani");
+ System.out.println("Added by sayani  ");
 
 		
 	 System.out.println("hello");
