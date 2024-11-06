c10548a70aaa767a4ecef8194b7e467228ae95c1
Soumyabrata Nandi <Soumyabrata Nandi@192.168.137.189>
2024-11-06 12:39:20 +0530

    added

--- ConcastCopyFramework/src/test/java/trialpackage/ProductPage.java
+++ ConcastCopyFramework/src/test/java/trialpackage/ProductPage.java
@@ -4,7 +4,8 @@ public class ProductPage {
 
 	public static void main(String[] args) {
 		System.out.println("i have added new product page");
-
+ 
+		System.out.println(" added by sayani");
 	}
 
 }
